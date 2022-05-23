920390a6a317e646becc400b5df0e7cd6eb29dcc
Carlos <carlos.msv@hotmail.com>
2022-05-22 23:53:33 -0300

    logs manuais

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const app = require('express')();
 const consign = require('consign');
+const { query } = require('express');
 const knex = require('knex');
 const knexfile = require('../knexfile');
-const knexLogger = require('knex-logger');
+//const knexLogger = require('knex-logger');
 
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
 
-app.use(knexLogger(app.db)); 
+//app.use(knexLogger(app.db)); 
 
 consign({ cwd: 'src' , verbose: false })
   .include('./config/middlewares.js')
@@ -18,5 +19,11 @@ consign({ cwd: 'src' , verbose: false })
 app.get('/', (req, res) => {
   res.status(200).send();
 })
+
+app.db.on('query', (query) => {
+  console.log({sql:query.sql, binding: query.bindings ? query.bindings.join(',') : ''});
+})
+  .on('query-response', (response) => console.log(response))
+  .on('error', error => console.log(error));
 
 module.exports = app;
