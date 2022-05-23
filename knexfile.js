1b46107fe0058ad16a9e5aaa9063e26b0264ebc1
Carlos <carlos.msv@hotmail.com>
2022-05-22 23:30:45 -0300

    adicionando postgres e knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '8.7',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'safePassword',
+      database: 'barriga'
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+  },
+}; 
---
+++ src/migrations/20220523022115_create_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.createTable('users', (t) => {
+    t.increments('id').primary();
+    t.string('name').notNull();
+    t.string('mail').notNull().unique();
+    t.string('passwd').notNull();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTable('users');
+};
