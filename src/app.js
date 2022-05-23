fbbd8aa894e5798eeab7486afaa2f9fd1dca1ade
Carlos <carlos.msv@hotmail.com>
2022-05-22 23:48:59 -0300

    knex-logger

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile');
+const knexLogger = require('knex-logger');
 
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
+
+app.use(knexLogger(app.db)); 
 
 consign({ cwd: 'src' , verbose: false })
   .include('./config/middlewares.js')
