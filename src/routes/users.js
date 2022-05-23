e9d18f6e39183a94fb6ef499590365a943e3111c
Carlos <carlos.msv@hotmail.com>
2022-05-23 00:48:27 -0300

    adicionando return no service de create users

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   const create = async (req, res) => {
     const result = await app.services.user.save(req.body)
     if(result.error) return res.status(400).json(result)
-    res.status(201).json(result[0]);
+    return res.status(201).json(result[0]);
   }; 
 
   return { findAll, create }
