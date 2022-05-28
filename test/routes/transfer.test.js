f1a167f677097737798ddaa7f6a0f74f7b647b17
Carlos Matheus Souza Viana <carlos.viana@squadra.com.br>
2022-05-27 21:17:54 -0300

    Deve listar apenas as transferências do usuário

--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -4,6 +4,10 @@ const app = require('../../src/app');
 const MAIN_ROUTE = '/v1/transfers'
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDAsIm5hbWUiOiJVc2VyICMxIiwibWFpbCI6InVzZXIxQG1haWwuY29tIn0.QMgvo_lPe0Rdxpx7cay_hIkDAbjCK_--VD2fP0NTTqk';
 
+beforeAll(async () => {
+  await app.db.seed.run();
+})
+
 test('Deve listar apenas as transferências do usuário', () => {
   return request(app).get(MAIN_ROUTE)
     .set('authorization', `bearer ${TOKEN}`)
