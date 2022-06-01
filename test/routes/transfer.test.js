bcf4e80bc9d76a28eb4993e1934f884fb6d95d36
Carlos <carlos.msv@hotmail.com>
2022-05-31 21:12:40 -0300

    Ao salvar uma transferência válida

--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -37,3 +37,46 @@ test('Deve inserir uma transferência com sucesso', () => {
 
     });
 });
+
+describe( 'Ao salvar uma transerência válida ...', () => {
+  let transferId;
+  let income;
+  let outcome;
+
+  test('Deve retornar o status 201 e os dados da transferência', () => {
+    return request(app).post(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`)
+      .send({description: "Regular Transfer", user_id: 10000, acc_ori_id: 10000, acc_dest_id: 10001, amount: 100, date: new Date()})
+      .then(async (res) => {
+        expect(res.status).toBe(201);
+        expect(res.body[0].description).toBe('Regular Transfer');
+        transferId = res.body[0].id;
+      })
+  });
+
+  test('As transações equivalentes devem ter sido geradas', async () => {
+    const transactions = await app.db('transactions').where({transfer_id: transferId}).orderBy('amount');
+    expect(transactions).toHaveLength(2);
+    [outcome, income] = transactions;
+  });
+
+  test('A transação de saída deve ser negativa', () => {
+    expect(outcome.description).toBe('Transfer to acc #10001')
+    expect(outcome.amount).toBe('-100.00')
+    expect(outcome.acc_id).toBe(10000)
+    expect(outcome.type).toBe('O') 
+  });
+
+  test('A transação de entrada deve ser positova', () => {
+    expect(income.description).toBe('Transfer from acc #10000')
+    expect(income.amount).toBe('100.00')
+    expect(income.acc_id).toBe(10001)
+    expect(income.type).toBe('I') 
+  });
+
+  test('Ambas devem referenciar a transferência que as originou', () => {
+    expect(income.transfer_id).toBe(transferId)
+    expect(outcome.transfer_id).toBe(transferId)
+    
+  });
+})
