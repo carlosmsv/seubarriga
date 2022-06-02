65624373f136473ccdca607b39acb3820e5c6ba3
Carlos <carlos.msv@hotmail.com>
2022-06-02 19:03:32 -0300

    Logar os eventos, protegendo os dados da aplicação do usuário

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const winston = require('winston')
+const uuid = require('uuidv4')
 const knexfile = require('../knexfile');
 
 // TODO criar chaveamento dinâmico
 if (process.env.NODE_ENV == 'prod') app.db = knex(knexfile.prod);
 else app.db = knex(knexfile.test);
 
-
+app.log = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({ format: winston.format.json({space: 1})}),
+    new winston.transports.File({ 
+      filename: 'logs/error.log', 
+      level: 'warn', 
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json({space: 1})),
+    }),
+  ]
+})
 consign({ cwd: 'src' , verbose: false })
   .include('./config/passport.js')
   .then('./config/middlewares.js')
@@ -17,6 +29,7 @@ consign({ cwd: 'src' , verbose: false })
   .into(app)
 
 app.get('/', (req, res) => {
+  app.log.debug('passei aqui')
   res.status(200).send();
 })
 
@@ -25,8 +38,10 @@ app.use((err, req, res, next) => {
   if ( name === 'ValidationError' ) res.status(400).json({ error: message });
   else if ( name === 'RecursoIndevidoError' ) res.status(403).json({ error: message });
   else {
-    console.log(message);
-    res.status(500).json({name, message, stack});
+    const id = uuid();
+    //console.log(message);
+    app.log.error({ id, name, message, stack })
+    res.status(500).json({id, error: 'Falha interna'});
   }
   next(err);
 })
