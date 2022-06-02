46e1612e7dd43f635ad276d9698fef63c6edf2b2
Carlos <carlos.msv@hotmail.com>
2022-06-02 19:45:55 -0300

    adicionando cors

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser')
 //const knexLogger = require('knex-logger')
+const cors = require('cors');
 
 module.exports = (app) => {
   app.use(bodyParser.json()); 
   //app.use(knexLogger.json()); 
+  app.use(cors({ origin: '*' }))
 }
